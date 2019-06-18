9734a3275d0f87d964068c3632e79ee98ecdf46b
apeden <apeden23@gmail.com>
2019-06-18 15:29:39 +0100

    first commit

---
+++ WesternBlotPlanner.py
@@ -0,0 +1,12 @@
+class WBRecord(object):
+    def __init(self, wb_num = "WB#XX.YYY"):
+        pass
+
+class Gel(object):
+    def __init(self, gel_num, maxLanes = 10):
+        pass
+
+class Lane(object):
+    def __init(self, samples, sample_vol, condition, final_vol = 20):
+        pass
+    
