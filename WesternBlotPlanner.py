cf48848255758f762d981992b7df20849c8a5faa
apeden <apeden23@gmail.com>
2019-06-20 17:02:16 +0100

    it's almost there

--- WesternBlotPlanner.py
+++ WesternBlotPlanner.py
@@ -13,18 +13,22 @@ class WBRecord(object):
             
 class Gel(object):
     gel_num = 0
-    def __init__(self, gel_num, maxLanes = 10):
-        assert(gel_num <6), "Too many gels"
-        self.index = ['A','B','C','D','E','F'][gel_num]
-        self.lanes = ()
-        self.gel_num = gel_num
+    def __init__(self, maxLanes = 10):
+        assert(Gel.gel_num <6), "Too many gels"
+        self.index = ['A','B','C','D','E','F'][Gel.gel_num]
+        self.laneNum = 2
+        self.lanes = []
+        self.gel_num = Gel.gel_num
         assert(len(self.lanes) <= maxLanes),"Too many lanes"
     def getIndex(self):
         return self.index
     def setLane(self, lane):
-        self.lanes += lane
+        assert(self.laneNum <= 10),"Too many lanes"
+        self.lanes.append(lane)
+        self.laneNum += 1
     def getLanes(self):
-        return self.lanes
+        for lane in self.lanes:
+            yield lane.__str__()
     def availLanes(self):
         return maxLanes - len(self.lanes)
     def getID(self):
@@ -33,12 +37,17 @@ class Gel(object):
 class Lane(object):
     def __init__(self, exNums, ruNums, condition, sample_vol,
                  final_vol, control = False):
+        self.num = 0
         self.exNums = exNums
         self.ruNums = ruNums
         self.condition = condition
         self.sample_vol = sample_vol
         self.control = control
         self.final_vol = final_vol
+    def setNum(self, num):
+        self.num = num
+    def getNum():
+        return self.num
     def getSamples(self):
         return self.samples
     def getCondition(self):
@@ -48,7 +57,7 @@ class Lane(object):
     def getControl(self):
         return self.control
     def __str__(self):
-        return str(self.exNums).ljust(20," ")\
+        return str(self.num).ljust(4, " ") + str(self.exNums).ljust(20," ")\
                + str(self.ruNums).ljust(20," ")+ \
                str(self.condition).ljust(15," ")+ str(self.sample_vol).ljust(7," ")+ \
                str(self.final_vol).ljust(3," ")
@@ -59,29 +68,61 @@ class FileReader(object):
         self.forLanes = []
         for l in self.inFile:
             self.forLanes.append(self.splitUp(l))
+        self.cntrLn = []
+        self.testLn = []
+        self.std = []
+        self.mrk = []
     def splitUp(self, line):
         return line.split(";")
     def getForLanes(self):
         return self.forLanes
+    def laneSort(self):      
+        laneType = None
+        for elem in self.forLanes:
+            if elem[0].lower() == "control":
+                laneType = self.cntrLn
+            elif elem[0].lower() == "standard":
+                laneType = self.std
+            elif elem[0].lower() == "marker":
+                laneType = self.mrk
+        if laneType == None:
+            for i in range(len(elem[2].split(","))):
+                self.testLn.append(Lane(elem[0], elem[1],
+                                        elem[2].split(",")[i],
+                                        elem[3].split(",")[i],
+                                        elem[4]))
+
+        else:
+            for i in range(len(elem[2].split(","))):
+                laneType.append(Lane(elem[1], elem[2],
+                                        elem[3].split(",")[i],
+                                        elem[4].split(",")[i],
+                                        elem[5]))
+
+
+
+            
+    def getLanes(self):
+        return self.mrk, self.cntrLn, self.testLn, self.std 
+    def close(self):
+        self.inFile.close()
     def __str__(self):
         return (str(self.forLanes))
 
 f = FileReader("plan.txt")
-print(f)
-cntrLn = []
-testLn = [] 
-laneTxts = f.getForLanes()
-for elem in laneTxts:
-    print(elem)
-    if elem[0].lower() == "control":
-        cntrLn.append(Lane(elem[1], elem[2], elem[3],
-                     elem[4], elem[5], control = True))
-    else:
-        testLn.append(Lane(elem[0], elem[1], elem[2],
-                     elem[3], elem[4]))
-for Ln in cntrLn:
-    print(Ln)
-for Ln in testLn:
-    print(Ln)
-
+f.laneSort()
+mrk, cntrLn, testLn, std = f.getLanes()
+g = Gel()
+num = 1
+for laneType in mrk, cntrLn, testLn, std :
+    for Ln in laneType:
+        Ln.setNum(num)
+        g.setLane(Ln)
+        num += 1
+    
+g.getLanes()
+print("Gel ", g.getIndex())
+for lane in g.getLanes():
+    print(lane)
+f.close()
         
