3100e489b57aef672ba5b6a3e7337b059123c870
apeden <apeden23@gmail.com>
2019-06-21 10:39:12 +0100

    now basing plan on some pseudocode I wrote in evernote

--- WesternBlotPlanner.py
+++ WesternBlotPlanner.py
@@ -1,39 +1,35 @@
 class WBRecord(object):
-    def __init__(self, date, gels, id = 'WB#XX.YYY',
-               head = ('Ex#', 'RU#', 'Cond', 'sample vol',
-                       'buffer', 'LB')):
-        self.date = date
-        self.gels = gels
+    def __init__(self, id = 'WB#XX.YYY'):
+        self.gels = []
         self.id = id
-        self.head = head
     def getID(self):
         return self.id
-    def getHead(self):
-        return self.head
+    def setGel(self, gel):
+        self.gels.append(gel)
+    def getGels(self):
+        for gel in self.gels:
+            yield gel
+    def __str__(self):
+        return self.id
             
 class Gel(object):
-    gel_num = 0
-    def __init__(self, maxLanes = 10):
-        assert(Gel.gel_num <6), "Too many gels"
+    def __init__(self, num, maxLanes = 10):
+        self.num = num
         self.maxLanes = maxLanes
-        self.index = ['A','B','C','D','E','F'][Gel.gel_num]
-        self.laneNum = 2
+        self.index = ['A','B','C','D','E','F'][self.num]
         self.lanes = []
-        self.gel_num = Gel.gel_num
-        assert(len(self.lanes) <= maxLanes),"Too many lanes"
+        assert(len(self.lanes) <= self.maxLanes),"Too many lanes"
     def getMaxLanes(self):
         return self.maxLanes
     def getIndex(self):
         return self.index
     def setLane(self, lane):
-        ##assert(self.laneNum <= 10),"Too many lanes"
         self.lanes.append(lane)
-        self.laneNum += 1
     def getLanes(self):
         for lane in self.lanes:
             yield lane.__str__()
     def availLanes(self):
-        return maxLanes - len(self.lanes)
+        return self.maxLanes - len(self.lanes)
     def getID(self):
         return self.gel_num
 
@@ -74,13 +70,11 @@ class FileReader(object):
         self.inFile = open(file)
         self.forLanes = []
         for l in self.inFile:
-            self.forLanes.append(self.splitUp(l))
+            self.forLanes.append(l.split(";"))
         self.cntrLn = []
         self.testLn = []
         self.std = []
         self.mrk = []
-    def splitUp(self, line):
-        return line.split(";")
     def getForLanes(self):
         return self.forLanes
     def laneSort(self):      
@@ -107,42 +101,50 @@ class FileReader(object):
         return (str(self.forLanes))
 
 class GelPlanner(object):
-    def__init__(self, file):
+    def __init__(self, file):
+        self.r = WBRecord()
         self.f = FileReader(file)
         self.f.laneSort()
         self.mrk, self.cntrLn, \
-        testLn, std = self.f.getLanes()        
-"plan.txt"
+        self.testLn, self.std = self.f.getLanes()
+        self.g = None
+        self.num = 1
+        self.lane_num = 1                     
+    def getRecord(self):
+        return self.r
+    def closeFile(self):
+        self.f.close()
+    def setGel(self):
+        self.g = Gel(self.num)
+        self.num += 1
+    def setLn(self, ln):
+        ln.setNum(self.lane_num)
+        self.g.setLane(ln)
+        self.lane_num += 1
+    def add_conlanes(self):
+        for laneType in self.mrk, self.cntrLn:
+            for ln in laneType:
+                self.setLn(ln)
+    def add_stdlanes(self):
+        for laneType in self.mrk, self.cntrLn:
+            for ln in laneType:
+                self.setLn(ln)
+    def add_testlanes(self):
+            for i in range(self.g.availLanes()):
+                try: self.setLn(self.testLn.pop(0))        
+                except: break                       
+    def __str__(self):
+        return str(self.r)
 
-f.laneSort()
 
-g = Gel()
-num = 1
-def setLn(Ln):
-Ln.setNum(num)
-    g.setLane(Ln)
-    num += 1
-    
-
-for laneType in mrk, cntrLn:
-    for Ln in laneType:
-        Ln.setNum(num)
-        g.setLane(Ln)
-        num += 1
-
-for i in range(g.getMaxLanes() - len(mrk) - len(std) -len(cntrLn)):
-    testLn[i].setNum(num)
-    g.setLane(testLn[i])
-    num += 1
-for Ln in std:
-    Ln.setNum(num)
-    g.setLane(Ln)
-    num += 1
-
-
-g.getLanes()
-print("Gel ", g.getIndex())
-for lane in g.getLanes():
-    print(lane)
-f.close()
+p = GelPlanner("plan.txt")
+p.setGel()
+p.add_conlanes()
+p.add_stdlanes()
+p.add_testlanes()
+print(p)
+gels_It = p.getRecord().getGels()
+for gel in gels_It:
+    gel.getLanes()
+p.closeFile()
         
