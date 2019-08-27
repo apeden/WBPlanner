c17222a5de1411afd3fdcea6204f4d80f209fd43
apeden <apeden23@gmail.com>
2019-08-27 23:58:33 +0100

    going further down this wormhole

--- WesternBlotPlanner.py
+++ WesternBlotPlanner.py
@@ -1,6 +1,7 @@
 from datetime import date
 today = date.today()
 d1 = today.strftime("%d/%m/%Y")
+import pylab
 
 
 class WBRecord(object):
@@ -80,6 +81,14 @@ class Lane(object):
         return self.tissue
     def getNumCond(self):
         return self.numCond
+    def getLaneComponents(self):
+        return [str(self.laneType),
+               str(self.exNums),
+               str(self.ruNums),
+               str(self.condition),
+               str(self.tissue),
+               str(self.sample_vol),
+               str(self.final_vol)]
     def __str__(self):
         return str(self.laneType).ljust(10,"_")\
                + str(self.exNums).ljust(25,"_")\
@@ -98,157 +107,164 @@ class FileReader(object):
         self.forLanes = []
         for l in self.inFile:
             self.forLanes.append(l.split(";"))
+        self.lanes = [] 
         self.dscrpt = self.forLanes[0][0]
         self.perfDate =self.forLanes[1][0]
-        self.cntrLn = []
-        self.testLn = []
-        self.std = []
-        self.mrk = []
     def getForLanes(self):
         return self.forLanes
     def laneSort(self):      
-        """Classify lanes and sort into separate lists"""
+        """Classify lanes and sort into separate dicts"""
         laneType = None
-        #forLanes: type;RU#;Tissues;Exs;Conditions;Volumes;FinalVol
-        #or:       type;Markers;Vol;None;None;None;FinalVol
-        for elem in self.forLanes[2:]:
-            typ = elem[0].lower()
-            if "control" in typ:
-                laneType = self.cntrLn
-            elif "marker" in typ:
-                laneType = self.mrk
-            elif "standard" in typ:
-                laneType = self.std
-            else: laneType = self.testLn 
-            for i in range(len(elem[2].split(","))):
-                for j in range(len(elem[4].split(","))):
+        for lineData in self.forLanes[2:]:
+            laneDict = {}
+            for i in range(len(lineData[2].split(","))):
+                for j in range(len(lineData[4].split(","))):
                     try:
-                        laneType.append(
-                            Lane(
-                                elem[0],
-                                elem[1],
-                                elem[2].split(",")[i],
-                                elem[3].split(",")[i],
-                                elem[4].split(",")[j],
-                                elem[5].split(",")[j],
-                                elem[6],
-                                len(elem[4].split(","))))
+                        laneDict["Type"] = lineData[0] 
+                        laneDict["Case"] = lineData[1]
+                        laneDict["Tissue"] = lineData[2].split(",")[i] 
+                        laneDict["Ex Num"] = lineData[3].split(",")[i]
+                        laneDict["Condition"] = lineData[4].split(",")[j]
+                        laneDict["Sample Volume"] = lineData[5].split(",")[j]
+                        laneDict["Final Vol"] = lineData[6]
                     except IndexError:
                         print("There must be a load volume stated",
                               " for each condition",
                               " and an extract number",
                               "  for each tissue")
                         raise
+            self.lanes.append(laneDict)
     def getDscrpt(self):
         return self.dscrpt
     def getPerfDate(self):
         return self.perfDate 
     def getLanes(self):
-        return self.mrk, self.cntrLn, self.testLn, self.std 
+        return self.lanes
     def close(self):
         self.inFile.close()
     def __str__(self):
         return (str(self.forLanes))
 
-class GelPlanner(object):
-    def __init__(self, directory, file):
-        self.r = WBRecord(file)
-        self.f = FileReader(directory, file+".txt")
-        self.f.laneSort()
-        self.mrk, self.cntrLn, \
-        self.testLn, self.std = self.f.getLanes()
-        self.g = None
-        self.num = 0
-    def getTestLns(self):
-        return self.testLn
-    def getRecord(self):
-        return self.r
-    def getDscrpt(self):
-        return self.f.getDscrpt()
-    def getPerfDate(self):
-        return self.f.getPerfDate()
-    def closeFile(self):
-        self.f.close()
-    def setGel(self):
-        self.g = Gel(self.num)
-        self.num += 1
-    def getGel(self):
-        return self.g
-    def setLn(self, ln):   
-        self.g.setLane(ln)
-    def add_conlanes(self):
-        for laneType in self.mrk, self.cntrLn:
-            for i in range(len(laneType)):
-                self.setLn(laneType[i])
-    def add_testlanes(self):
-        tissue = ""
-        availSpaces = self.g.getMaxLanes()\
-        -len(self.mrk)\
-        -len(self.cntrLn)\
-        -len(self.std)
-        #if there aren't enough spaces for all conditions, move on
-        i = 0
-        while(availSpaces-i>0) and (len(self.testLn) > 0):
-            if (self.testLn[0].getTissue() != tissue) \
-               and (self.testLn[0].getNumCond() > availSpaces-i):
-                break
-            tissue = self.testLn[0].getTissue()
-            self.setLn(self.testLn.pop(0))
-            i += 1
-    def add_stdlanes(self):
-        for i in range(len(self.std)):
-            self.setLn(self.std[i])
-    def buildGels(self):
-        while len(self.testLn) > 0:
-            self.setGel()
-            self.add_conlanes()
-            self.add_testlanes()
-            self.add_stdlanes()
-            self.addGel(self.getGel().copy())
-    def buildDiagGels(self, numStd = 4):
-        while len(self.testLn) > 0:
-            self.setGel()
-            n = 0
-            self.setLn(self.mrk[0])
-            while(len(self.testLn) > 0):
-                self.setLn(self.testLn.pop(0))
-                self.setLn(self.std[n]) 
-                n += 1
-            self.setLn(self.std[n])
-            self.addGel(self.getGel().copy())
-    def addGel(self, gel):
-        self.r.setGel(gel)
-    def __str__(self):
-        return str(self.r)
-
-def toTextFile(
-    file, directory = "plans", scrnPrnt = True, repeat =1, diag = False):
-    p = GelPlanner(directory,file)
-    memo = open(file+" lane order.txt", "w")
-    if scrnPrnt:
-        print(p.getRecord())
-        print(p.getDscrpt())
-        print("Performed on "+p.getPerfDate())
-    memo.write(p.getRecord().__str__()+"\n")
-    memo.write(p.getDscrpt()+"\n")
-    memo.write("Performed on "+p.getPerfDate())
-    if diag:
-        p.buildDiagGels() 
-    else:
-        p.buildGels()
-    gel_it = p.getRecord().getGelsIt()
-    for gel in gel_it:
-        if scrnPrnt: print(gel)
-        memo.write(gel.__str__()+"\n")
-        positn = 1
-        for lane in gel.getLanes():
-            if scrnPrnt: print(str(positn).ljust(3,"_"),lane)
-            memo.write(str(positn).ljust(3,"_")+str(lane)+"\n")
-            positn += 1
-    memo.close()            
-    p.closeFile()
-
-toTextFile("WB#19.089 AP (NaPTA - 21 23 24 25 28 29)")
-
-
-
+##class GelPlanner(object):
+##    def __init__(self, directory, file):
+##        self.r = WBRecord(file)
+##        self.f = FileReader(directory, file+".txt")
+##        self.f.laneSort()
+##        self.lanes = self.f.getLanes()
+##        self.g = None
+##        self.num = 0
+##    def getTestLns(self):
+##        return self.testLn
+##    def getRecord(self):
+##        return self.r
+##    def getDscrpt(self):
+##        return self.f.getDscrpt()
+##    def getPerfDate(self):
+##        return self.f.getPerfDate()
+##    def closeFile(self):
+##        self.f.close()
+##    def setGel(self):
+##        self.g = Gel(self.num)
+##        self.num += 1
+##    def getGel(self):
+##        return self.g
+##    def setLn(self, ln):   
+##        self.g.setLane(ln)
+##    def add_conlanes(self):
+##        for laneType in self.mrk, self.cntrLn:
+##            for i in range(len(laneType)):
+##                self.setLn(laneType[i])
+##    def add_testlanes(self):
+##        tissue = ""
+##        availSpaces = self.g.getMaxLanes()\
+##        -len(self.mrk)\
+##        -len(self.cntrLn)\
+##        -len(self.std)
+##        #if there aren't enough spaces for all conditions, move on
+##        i = 0
+##        while(availSpaces-i>0) and (len(self.testLn) > 0):
+##            if (self.testLn[0].getTissue() != tissue) \
+##               and (self.testLn[0].getNumCond() > availSpaces-i):
+##                break
+##            tissue = self.testLn[0].getTissue()
+##            self.setLn(self.testLn.pop(0))
+##            i += 1
+##    def add_stdlanes(self):
+##        for i in range(len(self.std)):
+##            self.setLn(self.std[i])
+##    def buildGels(self):
+##        while len(self.testLn) > 0:
+##            self.setGel()
+##            self.add_conlanes()
+##            self.add_testlanes()
+##            self.add_stdlanes()
+##            self.addGel(self.getGel().copy())
+##    def buildDiagGels(self, numStd = 4):
+##        while len(self.testLn) > 0:
+##            self.setGel()
+##            n = 0
+##            self.setLn(self.mrk[0])
+##            while(len(self.testLn) > 0):
+##                self.setLn(self.testLn.pop(0))
+##                self.setLn(self.std[n]) 
+##                n += 1
+##            self.setLn(self.std[n])
+##            self.addGel(self.getGel().copy())
+##    def addGel(self, gel):
+##        self.r.setGel(gel)
+##    def __str__(self):
+##        return str(self.r)
+##
+##def toTextFile(
+##    file, directory = "plans", scrnPrnt = True, repeat = 1, diag = False):
+##    p = GelPlanner(directory,file)
+##    memo = open(file+" lane order.txt", "w")
+##    if scrnPrnt:
+##        print(p.getRecord())
+##        print(p.getDscrpt())
+##        print("Performed on "+p.getPerfDate())
+##    memo.write(p.getRecord().__str__()+"\n")
+##    memo.write(p.getDscrpt()+"\n")
+##    memo.write("Performed on "+p.getPerfDate())
+##    if diag:
+##        p.buildDiagGels() 
+##    else:
+##        p.buildGels()
+##    gel_it = p.getRecord().getGelsIt()
+##    for gel in gel_it:
+##        rowLabels, columnLabels, tableVals = [],[],[]
+##        if scrnPrnt: print(gel)
+##        memo.write(gel.__str__()+"\n")
+##        positn = 1
+##        for lane in gel.getLanes():
+##            columnLabels = ["Lane",
+##                            "Type",
+##                            "Ex#",
+##                            "RU#",
+##                            "Cond",
+##                            "Tissue",
+##                            "Sample vol",
+##                            "Final vol"]
+##            rowLabels.append(positn)
+##            if scrnPrnt: print(str(positn).ljust(3,"_"),lane)
+##            lane.replace("_"," ")
+##            print(lane.__str__().split("_"," "))
+##            tableVals.append(lane.__str__().split("_",""))
+##            memo.write(str(positn).ljust(3,"_")+str(lane)+"\n")
+##            positn += 1
+##        #Produce table
+##        table = pylab.table(rowLabels = rowLabels,
+##                            colLabels = columnLabels,
+##                            cellText = tableVals,
+##                            cellLoc = 'center',
+##                            loc = 'center',
+##                            colWidths = [0.2]*8)
+##        table.scale(1, 2.5)
+##        pylab.title(gel.__str__())
+##    memo.close()            
+##    p.closeFile()
+##
+##toTextFile("WB#19.089 AP (NaPTA - 21 23 24 25 28 29)")
+##
+##
+##
